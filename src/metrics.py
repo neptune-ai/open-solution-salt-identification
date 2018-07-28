fixed metric calculation, dropped labeler from pipeline, added cumsum exploration to notebook

src/metrics.py
@@ -22,7 +22,9 @@ def compute_ious(gt, predictions):
     gt_ = get_segmentations(gt)
     predictions_ = get_segmentations(predictions)
 
-    if len(predictions_) == 0:
+    if len(gt_) == 0 and len(predictions_) == 0:
+        return np.ones((1, 1))
+    elif len(gt_) != 0 and len(predictions_) == 0:
         return np.zeros((1, 1))
     else:
         iscrowd = [0 for _ in predictions_]

src/models.py
@@ -161,7 +161,7 @@ class PyTorchUNet(Model):
                                     cross_entropy_loss=nn.CrossEntropyLoss(),
                                     dice_activation='softmax',
                                     dice_weight=self.architecture_config['model_params']['dice_weight'],
-                                    bce_weight=self.architecture_config['model_params']['bce_weight']
+                                    cross_entropy_weight=self.architecture_config['model_params']['bce_weight']
                                     )
         elif self.activation_func == 'sigmoid':
             loss_function = partial(mixed_dice_bce_loss,